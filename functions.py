3113db9a393c9b4d8c780ce11824a9576ba7b953
Ammar Ali <funnyguy118@gmail.com>
2019-09-27 12:41:13 -0400

    update function.py with outliers and strip methods. new pickle has been created

--- functions.py
+++ functions.py
@@ -1,21 +1,23 @@
 import numpy as np
 import pandas as pd
+from scipy import stats
 
 def replace_column_w_nan(df, list_of_columns, replace):
     for column in list_of_columns:
         df[column] = df[column].replace(to_replace=replace, value=np.nan)
     print("{}'s have been replaced with nan in all of these columns {}".format(replace, list_of_columns))
     
-
 def drop_na_rows(df, list_of_columns):
     # inplace=True is important to have. Without it, the .dropna() returns a NoneType Object
     df.dropna(axis=0, subset=list_of_columns, inplace=True)
     print("na rows from these columns ,{}, have been removed.".format(list_of_columns))
-    return df
 
 def drop_columns(df, list_of_columns):
     df.drop(axis=1, columns=list_of_columns, inplace = True)
     print("{} have been dropped".format(list_of_columns))
+
+def strip_spaces(df):
+    df = df.applymap(lambda x: x.strip() if type(x) == str else x)
 
 def check_duplicates(df):
     if df.duplicated().sum():
@@ -23,5 +25,19 @@ def check_duplicates(df):
     else:
         print("You're all clear")
 
-            
-    
+def get_z_score(df_column):
+    zscore_list = np.abs(stats.zscore(df_column))
+    return zscore_list
+
+def remove_outliers(df, list_of_columns):
+    for column in list_of_columns:
+        
+        df.reset_index(inplace=True,drop=True)
+        
+        column_zscore = get_z_score(df[column])
+        enumerated_column_z_score = enumerate(column_zscore)
+        
+        for index, element in enumerated_column_z_score:
+            if element > 3:
+                df = df.drop([index])
+    return df
