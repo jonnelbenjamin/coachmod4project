69a3b28b436d80c206f0fc604bec763056ca7d31
Ammar Ali <funnyguy118@gmail.com>
2019-10-02 16:28:02 -0400

    explore new methods of removing outliers and new methods of dummying our categorical features

--- functions.py
+++ functions.py
@@ -24,23 +24,37 @@ def check_duplicates(df):
         print("there are duplicates")
     else:
         print("You're all clear of duplicates")
-
-def remove_outliers(df, list_of_columns, zthreshold=3):
-    """zthreshold is an optional parameter default set to 3"""
-    for column in list_of_columns:
+        
+def remove_outliers(df, col_names):
+    for col in col_names:
         df.reset_index(inplace=True,drop=True)
-        column_zscore = get_z_score(df[column])
-        df = drop_high_zscores(df, column_zscore, zthreshold)
-    print("Removed outliers from these columns:{}".format(list_of_columns))
+        q1 = df[col].quantile(0.25)
+        q3 = df[col].quantile(0.75)
+        iqr = q3-q1 #Interquartile range
+        fence = 1.5*iqr
+        fence_low = q1-fence
+        fence_high = q3+fence
+        df = df.loc[(df[col] > fence_low) & (df[col] < fence_high)]
     return df
 
-def get_z_score(df_column):
-    zscore_list = np.abs(stats.zscore(df_column))
-    return enumerate(zscore_list)
+# Useful function, but incorrectly used in this project.
+# 
+# def remove_outliers(df, list_of_columns, zthreshold=3):
+#     """zthreshold is an optional parameter default set to 3"""
+#     for column in list_of_columns:
+#         df.reset_index(inplace=True,drop=True)
+#         column_zscore = get_z_score(df[column])
+#         df = drop_high_zscores(df, column_zscore, zthreshold)
+#     print("Removed outliers from these columns:{}".format(list_of_columns))
+#     return df
 
-def drop_high_zscores(df, enumerated_zscores,zthreshold):
-    for index, element in enumerated_zscores:
-        if element > zthreshold:
-            df = df.drop([index])
-    return df
+# def get_z_score(df_column):
+#     zscore_list = np.abs(stats.zscore(df_column))
+#     return enumerate(zscore_list)
+
+# def drop_high_zscores(df, enumerated_zscores,zthreshold):
+#     for index, element in enumerated_zscores:
+#         if element > zthreshold:
+#             df = df.drop([index])
+#     return df
 
